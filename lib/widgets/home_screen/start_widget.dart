1d1858e006c75a065b542994e682fe3c718fdfa7
Athulolikkal <athulolikkal@gmail.com>
2024-03-26 18:24:12 +0530

    working on start container

--- lib/widgets/home_screen/start_widget.dart
+++ lib/widgets/home_screen/start_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class StartContainerWidget extends StatelessWidget {
   const StartContainerWidget({super.key});
@@ -6,36 +7,52 @@ class StartContainerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      padding: EdgeInsets.symmetric(vertical: 12.0),
       child: Container(
         height: 250,
         margin: const EdgeInsets.all(12.0),
         decoration: BoxDecoration(
-            color: Color.fromARGB(255, 233, 244, 252),
-            borderRadius: BorderRadius.circular(10)),
+            color: const Color.fromARGB(255, 233, 244, 252),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromARGB(255, 10, 10, 10).withOpacity(0.1), // Shadow color
+                spreadRadius: 2, // Spread radius
+                blurRadius: 1, // Blur radius
+                offset: Offset(0,1), // Offset
+              ),
+            ]),
         child: Padding(
           padding: EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Text(
-                    '''Can You Get These\nRight ?''',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Image.asset('assets/images/online_education.png',width: 140,height: 140,)
-                ],
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text(
+              '''Can You Get These\nRight ?''',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                child: Text('Take the quiz to find out '),
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Text('Take the quiz to find out ')),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 25.0),
+              child: OutlinedButton(
+                onPressed: () {},
+                child: Text(
+                  'Start',
+                  style: TextStyle(fontSize: 20),
+                ),
+                style: ButtonStyle(
+                  side: MaterialStatePropertyAll(
+                      BorderSide(color: Colors.blue, width: 3)),
+                  foregroundColor: MaterialStateProperty.all(Colors.blue),
+                  minimumSize: MaterialStateProperty.all(Size(100, 50)),
+                ),
               ),
-            ],
-          ),
+            )
+          ]),
         ),
       ),
     );
